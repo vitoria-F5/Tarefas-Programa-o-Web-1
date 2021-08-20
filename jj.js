6b23fefa43671b49d3e523e5d73a11928ca76df6
Vitória Isabel <84388956+vitoria-F5@users.noreply.github.com>
2021-08-20 12:26:41 -0300

    Add files via upload

---
+++ jj.js
@@ -0,0 +1,9 @@
+function aumentar(fontSize) {
+    h2 = document.querySelector("h2")
+    h2.style.fontSize = "40px";
+  }
+
+function diminuir(fontSize) {
+h2 = document.querySelector("h2")
+h2.style.fontSize = "35px";
+}
