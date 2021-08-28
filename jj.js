ac20293b2b34d8ff4c053852e26a6bd83f6de0e0
Vitória Isabel <84388956+vitoria-F5@users.noreply.github.com>
2021-08-28 16:53:39 -0300

    Add files via upload

--- jj.js
+++ jj.js
@@ -1,9 +1,30 @@
-function aumentar(fontSize) {
-    h2 = document.querySelector("h2")
-    h2.style.fontSize = "40px";
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  inserirBotoesComInnerHTML();
+  inserirBotaoJS();
+});
 
 function diminuir(fontSize) {
-h2 = document.querySelector("h2")
-h2.style.fontSize = "35px";
+  h2 = document.querySelector("h2")
+  h2.style.fontSize = "35px";
+}
+function aumentar(fontSize) {
+  h2 = document.querySelector("h2")
+  h2.style.fontSize = "40px";
+}
+
+function inserirBotaoJS() {
+  div = document.createElement("div");
+ 
+  div.appendChild(botao);
+  document.body.prepend(div);
+}
+
+function inserirBotoesComInnerHTML() {
+  div = document.createElement("div");
+  div.style.backgroundColor = "cyan";
+  div.innerHTML = `
+  <button onclick="aumentar('fontSize')">+</button>
+  <button onclick="diminuir('fontSize')">-</button>
+  `;
+  document.body.prepend(div);
 }
